2334faac19551eb2ac16478589a78a59cbc5ddd9
ThyTran <huethytran25@gmail.com>
2020-01-04 17:36:20 +0700

    test

--- src/containers/UserAvatar.js
+++ src/containers/UserAvatar.js
@@ -3,7 +3,8 @@ import UserAvatar from '../views/ProfilePage/Sections/UserAvatar';
 import * as actions from '../actions/index';
 
 const mapStateToProps = state => ({
-  avatar: state.user.avatar
+  avatar: state.user.avatar,
+  fullName: state.user.fullName
 });
 const mapDispatchToProps = dispatch=>{
   return {
